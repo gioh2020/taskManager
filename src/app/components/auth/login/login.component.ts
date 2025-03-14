447d5151f1e780e0398e3b7f3026524de5d992be
Giovanny <Development.Jr1@myasofty.com>
2025-03-13 23:30:34 -0500

    Added login logic

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -8,46 +10,71 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
-  loginForm!: FormGroup;
+  loginForm: FormGroup;
   submitted = false;
+  isLoading = false;
+  successful = false;
+  errorMessage = '';
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
-  ) { }
-
-  ngOnInit(): void {
+    private router: Router,
+    private authService: AuthService
+  ) {
     this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
 
-
-  get form() { return this.loginForm.controls; }
-
   onSubmit(): void {
     this.submitted = true;
-
+    this.errorMessage = '';
+    this.successful = false;
 
     if (this.loginForm.invalid) {
       return;
     }
 
-    console.log('Login successful', this.loginForm.value);
+    const { username, password } = this.loginForm.value;
+    this.isLoading = true;
 
+    this.authService.login(username, password)
+      .pipe(
+        tap(() => {
+          this.isLoading = false;
+          this.successful = true;
+          this.loginForm.disable();
 
-    // this.router.navigate(['/dashboard']);
+        }),
+        catchError((error) => {
+          this.isLoading = false;
+          if (error.status === 401) {
+            if (error.error === "Usuario no encontrado") {
+              this.loginForm.get('username')?.setErrors({ notFound: true });
+              this.errorMessage = 'El usuario no existe';
+            } else if (error.error === "Contraseña incorrecta") {
+              this.loginForm.get('password')?.setErrors({ incorrect: true });
+              this.errorMessage = 'La contraseña es incorrecta';
+            } else {
+              this.errorMessage = 'Credenciales inválidas';
+            }
+          } else {
+            this.errorMessage = 'Error al intentar iniciar sesión';
+          }
+          
+           return throwError(() => error);
+         
+        })
+
+      )
+      .subscribe((res) =>
+        this.router.navigate(['/page/taskManager'])
+      );
   }
 
-
-
-
   goToSignup(): void {
-
     console.log('Navigate to signup');
     // this.router.navigate(['/signup']);
   }
-
 }
