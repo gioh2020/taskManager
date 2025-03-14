7467105c0e00e4edf0208d431060cd3c0b7390b8
Giovanny <Development.Jr1@myasofty.com>
2025-03-13 23:28:58 -0500

    add routes taskmanager and auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
+  } ,
+  {
+    path: 'page',
+    loadChildren: () => import('./components/taskManager/task-manager.module').then(m => m.TaskManagerModule)
+  } ,
+  {
+    path: '**',
+    redirectTo: 'auth'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
