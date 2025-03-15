b85511fa9dba7966e8629ea06a593f81d24032e6
Giovanny <Development.Jr1@myasofty.com>
2025-03-15 08:46:37 -0500

    add Subscription and ngOnDestroy

--- front/src/app/components/taskManager/taskManagerTable/task-manager-table.component.ts
+++ front/src/app/components/taskManager/taskManagerTable/task-manager-table.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Task, TaskManagerService } from '../services/task-manager-servise.service';
 import { User } from '../../auth/interface';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-task-manager-table',
@@ -8,9 +9,11 @@ import { User } from '../../auth/interface';
   styleUrls: ['./task-manager-table.component.css']
 })
 export class TaskManagerComponentTable {
-  taskbyUser: Task[] = [];
    userDataString: string | null;
     userData: User;
+  taskbyUser$!: Subscription ;
+  taskbyUser: Task[] = [] ;
+    
   constructor(private taskService: TaskManagerService,) {
     this.userDataString = sessionStorage.getItem('user') || '';
 
@@ -18,18 +21,31 @@ export class TaskManagerComponentTable {
   }
 
   ngOnInit(){
-    this.taskService.userTask.subscribe((res)=>{
+   this.taskbyUser$ = this.taskService.userTask.subscribe((res)=>{
+      this.taskbyUser = res
+    })
+  }
+  getUserTasks(){
+    this.taskService.getUserTasks(Number(this.userData.id)).subscribe((res) => {
       this.taskbyUser = res
     })
   }
   onDelete(task : Task){
     const {id, userId} = task
-    this.taskService.deleteTask(id, userId).pipe().subscribe()
+    this.taskService.deleteTask(id, userId).pipe(
+      tap(() => this.getUserTasks())
+    ).subscribe((res)=> {
+      
+    })
   }
   onChangeTask(task : Task, event : Event){
     const newStatus = (event.target as HTMLSelectElement).value;
     task.status = newStatus
     this.taskService.updateTask(task).pipe().subscribe()
+  }
+
+  ngOnDestroy(){
+    this.taskbyUser$.unsubscribe()
   }
 
 }
