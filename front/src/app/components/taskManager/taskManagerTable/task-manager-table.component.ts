e10c6750643d73ce1f750c679b14e9a07fc40d6c
Giovanny <Development.Jr1@myasofty.com>
2025-03-15 08:11:17 -0500

    add logical an new styles

--- front/src/app/components/taskManager/taskManagerTable/task-manager-table.component.ts
+++ front/src/app/components/taskManager/taskManagerTable/task-manager-table.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Task, TaskManagerService } from '../services/task-manager-servise.service';
+import { User } from '../../auth/interface';
 
 @Component({
   selector: 'app-task-manager-table',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-manager-table.component.css']
 })
 export class TaskManagerComponentTable {
+  taskbyUser: Task[] = [];
+   userDataString: string | null;
+    userData: User;
+  constructor(private taskService: TaskManagerService,) {
+    this.userDataString = sessionStorage.getItem('user') || '';
+
+    this.userData = JSON.parse(this.userDataString);
+  }
+
+  ngOnInit(){
+    this.taskService.userTask.subscribe((res)=>{
+      this.taskbyUser = res
+    })
+  }
+  onDelete(task : Task){
+    const {id, userId} = task
+    this.taskService.deleteTask(id, userId).pipe().subscribe()
+  }
+  onChangeTask(task : Task, event : Event){
+    const newStatus = (event.target as HTMLSelectElement).value;
+    task.status = newStatus
+    this.taskService.updateTask(task).pipe().subscribe()
+  }
 
 }
